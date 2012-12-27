8ce310891a55f22b50afe1a71f7e08fe8b21958b
Sam Agnew <sam@sam-Studio-1555.(none)>
2012-12-27 02:56:02 -0500

    Should be running now

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def perform_script():
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT",5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(host='0.0.0.0', port=port)
 
