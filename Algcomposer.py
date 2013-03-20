ff2a53aebc70add3d1e7365a56962b04ccb091ae
Sam Agnew <sagnew@eden.rutgers.edu>
2013-03-20 16:57:52 -0400

    Fixed indentation

--- Algcomposer.py
+++ Algcomposer.py
@@ -13,191 +13,191 @@ Output: Another guitar tab text file that is modified to be a different composit
 """
 
 def get_scale(roots, steps):
-	"""
-	Returns a 2D list of each note on each string that is in a specific key.
+ """
+ Returns a 2D list of each note on each string that is in a specific key.
 
-	Arguments:
-	roots: A list of root notes of the scale corresponding to where this note is on each string
-	steps: The order in which the scale steps(whole steps and half steps)
+ Arguments:
+ roots: A list of root notes of the scale corresponding to where this note is on each string
+ steps: The order in which the scale steps(whole steps and half steps)
 
-	Example: root = 5, 0, 7, 2, 9, 5, steps = 2212221 would correspond to an A major scale.
-	"""
+ Example: root = 5, 0, 7, 2, 9, 5, steps = 2212221 would correspond to an A major scale.
+ """
 
-	scale = []
-	for note in roots:
-		scale.append([note])
+ scale = []
+ for note in roots:
+ 	scale.append([note])
 
-	#This loop structure goes through each string, and ascends the scale all the way up to fret 24
-	for string in scale:
-		num = string[0]
-		while num <= 24:
-			for step in steps:
-				num += step
-				if num > 24:
-					break
-				string.append(num)
+ #This loop structure goes through each string, and ascends the scale all the way up to fret 24
+ for string in scale:
+ 	num = string[0]
+ 	while num <= 24:
+ 		for step in steps:
+ 			num += step
+ 			if num > 24:
+ 				break
+ 			string.append(num)
 
-	#This loop does the same thing, but from the root note down to the open string(fret 0)
-	for string in scale:
-		num = string[0]
-		while num >= 0:
-			for step in steps[::-1]:
-				num -= step
-				if num < 0:
-					break
-				string.insert(0, num)
+ #This loop does the same thing, but from the root note down to the open string(fret 0)
+ for string in scale:
+ 	num = string[0]
+ 	while num >= 0:
+ 		for step in steps[::-1]:
+ 			num -= step
+ 			if num < 0:
+ 				break
+ 			string.insert(0, num)
 
-	return scale
+ return scale
 
 def get_scale_steps(name_of_scale):
-	"""
-	Returns a list of the steps in the given scale
+ """
+ Returns a list of the steps in the given scale
 
-	To be used to interpret user input.
-	"""
+ To be used to interpret user input.
+ """
 
-	scales = {"minor": [2,1,2,2,1,2,2],
-			  "major": [2,2,1,2,2,2,1],
-			  "harmonic": [2,1,2,2,1,3,1],
-			  "dorian": [2,1,2,2,2,1,2],
-			  "mixolydian": [2,2,1,2,2,1,2],
-			  "pentatonic": [3,2,2,3,2]
-			}
+ scales = {"minor": [2,1,2,2,1,2,2],
+ 		  "major": [2,2,1,2,2,2,1],
+ 		  "harmonic": [2,1,2,2,1,3,1],
+ 		  "dorian": [2,1,2,2,2,1,2],
+ 		  "mixolydian": [2,2,1,2,2,1,2],
+ 		  "pentatonic": [3,2,2,3,2]
+ 		}
 
-	return scales[name_of_scale]
+ return scales[name_of_scale]
 
 def get_root_note_positions(root):
-	"""
-	Returns a list of the root note position on each string for every note
-	"""
+ """
+ Returns a list of the root note position on each string for every note
+ """
 
-	first_positions = {"e": 0,
-					   "f": 1,
-					   "gf": 2,
-					   "g": 3,
-					   "af": 4,
-					   "a": 5,
-					   "bf": 6,
-					   "b": 7,
-					   "c": 8,
-					   "df": 9,
-					   "d": 10,
-					   "ef": 11
-					}
-	first = first_positions[root]
-	root_positions = [first, first + 7, first + 2, first + 9, first + 5, first]
+ first_positions = {"e": 0,
+ 				   "f": 1,
+ 				   "gf": 2,
+ 				   "g": 3,
+ 				   "af": 4,
+ 				   "a": 5,
+ 				   "bf": 6,
+ 				   "b": 7,
+ 				   "c": 8,
+ 				   "df": 9,
+ 				   "d": 10,
+ 				   "ef": 11
+ 				}
+ first = first_positions[root]
+ root_positions = [first, first + 7, first + 2, first + 9, first + 5, first]
 
-	return root_positions
+ return root_positions
 
 def strip_empty_lines(str):
-	"""
-	Takes an empty string and removes lines that contain only whitespace
-	"""
+ """
+ Takes an empty string and removes lines that contain only whitespace
+ """
 
-	final_str = ""
-	for line in str.split('\n'):
-		if not line.isspace() and len(line) > 0:
-			final_str += line + '\n'
+ final_str = ""
+ for line in str.split('\n'):
+ 	if not line.isspace() and len(line) > 0:
+ 		final_str += line + '\n'
 
-	return """%s """ % final_str
+ return """%s """ % final_str
 
 
 
 def print_scale(scale):
-	"""
-	Prints a representation of the neck of the guitar for this specific scale
-	"""
+ """
+ Prints a representation of the neck of the guitar for this specific scale
+ """
 
-	for i in scale:
-		print i
+ for i in scale:
+ 	print i
 
 def change_key(note, scale, string, accidental=-1):
-	"""
-	This takes an input sequence of notes(guitar tablature) and changes it to a different key
+ """
+ This takes an input sequence of notes(guitar tablature) and changes it to a different key
 
-	The simplest algorithm I will implement for composing music.
-	Finds each note in the sequence and changes it to the closest note in the scale
-	This keeps the melody very similar, but changes what key it is in.
-	The melody may not be exactly the same because notes relative to each other will
-	not always be preserved the same way.
-	"""
+ The simplest algorithm I will implement for composing music.
+ Finds each note in the sequence and changes it to the closest note in the scale
+ This keeps the melody very similar, but changes what key it is in.
+ The melody may not be exactly the same because notes relative to each other will
+ not always be preserved the same way.
+ """
 
-	random_offset = 0
-	min = 24
-	min_fret = 24
-	for fret in scale[string]:
-		if abs(fret - note) < min:
-			min = abs(fret - note)
-			min_fret = fret
+ random_offset = 0
+ min = 24
+ min_fret = 24
+ for fret in scale[string]:
+ 	if abs(fret - note) < min:
+ 		min = abs(fret - note)
+ 		min_fret = fret
 
-	if accidental > 0:
-		rand_num = random.randrange(0, accidental, 1)
-		if rand_num == 1:
-			rand_num = random.randrange(0,1,1)
-			if rand_num == 1:
-				random_offset = 1
-			else:
-				random_offset = -1
+ if accidental > 0:
+ 	rand_num = random.randrange(0, accidental, 1)
+ 	if rand_num == 1:
+ 		rand_num = random.randrange(0,1,1)
+ 		if rand_num == 1:
+ 			random_offset = 1
+ 		else:
+ 			random_offset = -1
 
-	return min_fret + random_offset
+ return min_fret + random_offset
 
 def go_through_file(song, scale, method, accidental=-1):
-	"""
-	Goes through the input file(a song represented as guitar tablature) and performs the desired operation
+ """
+ Goes through the input file(a song represented as guitar tablature) and performs the desired operation
 
-	Arguments
-	song - the file to be parsed
-	scale - the scale being used
-	method - What the user wants to do with this song
-	accidental - The amount of randomness the user wishes to add(explained below)
+ Arguments
+ song - the file to be parsed
+ scale - the scale being used
+ method - What the user wants to do with this song
+ accidental - The amount of randomness the user wishes to add(explained below)
 
-	If the user wants to add an element of randomness, he or she may choose to include the last argument.
-	This will give a 1/accidental chance of a randomly placed accidental note being returned.
+ If the user wants to add an element of randomness, he or she may choose to include the last argument.
+ This will give a 1/accidental chance of a randomly placed accidental note being returned.
 
-	Methods so far: 'ck' = changes what key the song is in
+ Methods so far: 'ck' = changes what key the song is in
 
-	"""
+ """
 
-	modified_song = ""
-	string = 5
-	previous_note = '-'
-	song = strip_empty_lines(song)
+ modified_song = ""
+ string = 5
+ previous_note = '-'
+ song = strip_empty_lines(song)
 
-	for note in song:
-		if string < 0:
-			modified_song += '\n'
-			string = 5
+ for note in song:
+ 	if string < 0:
+ 		modified_song += '\n'
+ 		string = 5
 
-		if note.isdigit():
-			if previous_note.isdigit():
-				note = previous_note + note
-				note = int(note)
-				previous_note = '-'
-				if method == 'ck':
-					str_to_add = str(change_key(note, scale, string, accidental))
-					if len(str_to_add)>1:
-						modified_song += str_to_add
-					else:
-						modified_song += str_to_add + '-'
-			else:
-				previous_note = note
-		else:
-			if previous_note.isdigit():
-				non_note_char = note
-				note = int(previous_note)
-				previous_note = '-'
-				if method == 'ck':
-					modified_song += str(change_key(note, scale, string, accidental)) + non_note_char
-			else:
-				if note != '\n':
-					modified_song += note
-		if note == '\n':
-			previous_note = '-'
-			modified_song += '\n'
-			string -= 1
+ 	if note.isdigit():
+ 		if previous_note.isdigit():
+ 			note = previous_note + note
+ 			note = int(note)
+ 			previous_note = '-'
+ 			if method == 'ck':
+ 				str_to_add = str(change_key(note, scale, string, accidental))
+ 				if len(str_to_add)>1:
+ 					modified_song += str_to_add
+ 				else:
+ 					modified_song += str_to_add + '-'
+ 		else:
+ 			previous_note = note
+ 	else:
+ 		if previous_note.isdigit():
+ 			non_note_char = note
+ 			note = int(previous_note)
+ 			previous_note = '-'
+ 			if method == 'ck':
+ 				modified_song += str(change_key(note, scale, string, accidental)) + non_note_char
+ 		else:
+ 			if note != '\n':
+ 				modified_song += note
+ 	if note == '\n':
+ 		previous_note = '-'
+ 		modified_song += '\n'
+ 		string -= 1
 
-	if previous_note.isdigit():
-		if method == 'ck':
-			modified_song += str(change_key(int(previous_note), scale, string, accidental))
+ if previous_note.isdigit():
+ 	if method == 'ck':
+ 		modified_song += str(change_key(int(previous_note), scale, string, accidental))
 
-	return modified_song
+ return modified_song
--- app.py
+++ app.py
@@ -9,49 +9,49 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-	return render_template('main_page.html')
+    return render_template('main_page.html')
 
 @app.route('/submit', methods=['POST', 'GET'])
 def perform_script():
 
-	#Initialize variables
-	output = ""
-	option = request.form["options"]
-	root = request.form["root"]
-	scale = request.form["scale"]
-	song = """%s""" % request.form["guitar_tab"]
+    #Initialize variables
+    output = ""
+    option = request.form["options"]
+    root = request.form["root"]
+    scale = request.form["scale"]
+    song = """%s""" % request.form["guitar_tab"]
 
-	#Initializes the scale chosen by the user
-	jumps = get_scale_steps(scale)
-	roots = get_root_note_positions(root)
+    #Initializes the scale chosen by the user
+    jumps = get_scale_steps(scale)
+    roots = get_root_note_positions(root)
 
-	#Executes the method chosen by the user
-	if option == 'ck':
-		output = go_through_file(song, get_scale(roots, jumps), option)
-	if option == 'ckr':
-		output = go_through_file(song, get_scale(roots, jumps), 'ck', 5)
+    #Executes the method chosen by the user
+    if option == 'ck':
+        output = go_through_file(song, get_scale(roots, jumps), option)
+    if option == 'ckr':
+        output = go_through_file(song, get_scale(roots, jumps), 'ck', 5)
 
-	with open("templates/main_page.html") as main_page:
+    with open("templates/main_page.html") as main_page:
 
-		#Setting up variables for replacing
-		scale_text = "<option value=" + '"' + scale + '"'
-		root_text = "<option value=" + '"' + root + '"'
-		method_text = "<option value=" + '"' + option + '"'
-		input_text = "Enter text-based guitar tablature."
-		
-		#replace form elements in the original html file to preserve input
-		replacement = main_page.read().replace("View output here", output)
-		replacement = replacement.replace(input_text, song)
-		replacement = replacement.replace(scale_text, scale_text + "selected")
-		replacement = replacement.replace(root_text, root_text + "selected")
-		replacement = replacement.replace(method_text, method_text + "selected")
-		with open("templates/submitted.html", 'w') as output_file:
-			#Write to submitted.html
-			output_file.write(replacement)
+        #Setting up variables for replacing
+        scale_text = "<option value=" + '"' + scale + '"'
+        root_text = "<option value=" + '"' + root + '"'
+        method_text = "<option value=" + '"' + option + '"'
+        input_text = "Enter text-based guitar tablature."
 
-	return render_template('submitted.html') 
+        #replace form elements in the original html file to preserve input
+        replacement = main_page.read().replace("View output here", output)
+        replacement = replacement.replace(input_text, song)
+        replacement = replacement.replace(scale_text, scale_text + "selected")
+        replacement = replacement.replace(root_text, root_text + "selected")
+        replacement = replacement.replace(method_text, method_text + "selected")
+        with open("templates/submitted.html", 'w') as output_file:
+            #Write to submitted.html
+            output_file.write(replacement)
+
+    return render_template('submitted.html')
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT",5000))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0', port=port)
 
