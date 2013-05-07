ac69e1b5ace1563436a552104269df035770bfe7
Sam Agnew <sagnew@eden.rutgers.edu>
2013-05-07 13:24:09 -0400

    Changed main page to index in app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST', 'GET'])
 def perform_script():
